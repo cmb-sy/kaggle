1847ce98ceb089e9a0f687efc0f5a8e6888cbfc9
nakashima <nakashima@nakashimanoMacBook-Air.local>
2022-05-10 21:37:39 +0900

    add datasets output code

--- datasets/datasets_confirm.py
+++ datasets/datasets_confirm.py
@@ -2,6 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+df_train = pd.read_csv("train.csv")
+de_test =pd.read_csv('train.csv')
 
-df = pd.read_csv('train.csv')
-df_test = pd.read_csv('test.csv')
+#コラムの表示
+# print(df_train.columns)
+
+#データフレームの出力
+# print(df_train.head())
+
+# 統計量の出力
+# print(df_train.describe())
+
+# 欠損値の確認
+# df.isnull().sum()
