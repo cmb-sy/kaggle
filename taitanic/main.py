408d65368b16c7e657dc39044714d08d12e1a570
nakashima <nakashima@nakashimanoMacBook-Air.local>
2022-05-16 20:33:46 +0900

    make

--- taitanic/main.py
+++ taitanic/main.py
@@ -4,7 +4,8 @@ from sklearn.ensemble import RandomForestClassifier as rfc
 import pandas as pd
 from prior_process import prior_process as pp
 import sys
-sys.path.append("library")
+sys.path.append("../")
+from library.change_submit_file.change_submit_file import change_submit_file
 
 def random_forest(train_data, test_data):
   # n_estimator : 決定木数の設定。大きい方が精度が良いが（多くの多数決が可能）時間とメモリとのトレードオフ関係,defalut=100
@@ -26,10 +27,7 @@ def random_forest(train_data, test_data):
   # 学習したモデルで予測
   predict = rc.predict(test_features)
 
-
-  submit = pd.DataFrame({'PassengerId': df_test["PassengerId"], 'Survived': predict})
-  submit.to_csv("submit.csv", header=True, index=False)
-  print("Your submission was successfully saved!")
+  change_submit_file("PassengerId", "Survived", df_test, predict)
   
 if __name__ == '__main__':
   df_train = pd.read_csv("train.csv")
