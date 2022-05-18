f52f84bba43fa6b7a8a9792228a17ffc18dad192
nakashima <nakashima@nakashimanoMacBook-Air.local>
2022-05-18 21:35:11 +0900

    reserve file open code

--- house-prices-advanced/confirm.py
+++ house-prices-advanced/confirm.py
@@ -4,26 +4,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # index_colは
-def data_output(train, test, purpose_value):
+def visualizer(train, test, purpose_value):
 #目的変数のヒストグラム
   purpose_value = sns.histplot(train[purpose_value]) 
+  purpose_value.set_title("histgram of purpose value")
   tmp = purpose_value.get_figure()
   tmp.savefig('purpose_calue_histgram.png',  orientation="landscape")
 
 
-# plt.show()
-# 売却価格の概要をみてみる
-# print(df_train["SalePrice"].describe()
-# 歪度は正規分布に比べてどれくらい偏りがあるか
-# print(f"歪度: {round(df_train['SalePrice'].skew(),4)}" )
-# 尖度は正規分布に比べての尖り具合
-# print(f"尖度: {round(df_train['SalePrice'].kurt(),4)}" )
+# 目的変数の概要
+  file = open('purpose_value.txt', 'w')
+  file.write("概要\n")
+  file.write(str(train["SalePrice"].describe()))
+  file.write("\n\n尖度と歪度（どれくらい正規分布から離れているか）\n")
+  file.write(f"歪度: {round(train['SalePrice'].skew(),4)}\n" )
+  file.write(f"尖度: {round(train['SalePrice'].kurt(),4)}" )
+  file.close()
 
 # 欠損値の割合を見る
 # print(df_train.isnull().sum())
   # plt.savefig("histgram")
-  # pd.set_option("display.max_columns", 100)
-  # file = open('date_frame_train.txt', 'w')
+  # pd.set_option("display.max_columns", 50)
+  # file = open('frame_train.txt', 'w')
+  # file.write("\n")
   # file.write(str(df_train.head()))
   # file.close
 
@@ -37,5 +40,5 @@ if __name__ == '__main__':
   df_train = pd.read_csv("train.csv")
   # print(df_train['SalePrice'])
   df_test =pd.read_csv("test.csv")
-  data_output(df_train, df_test, "SalePrice")
+  visualizer(df_train, df_test, "SalePrice")
 
