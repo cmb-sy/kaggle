3689e5e6adffc4f854bb74dfed9a63214f58f9ae
nakashima <nakashima@nakashimanoMacBook-Air.local>
2022-05-19 21:25:21 +0900

    revise error code of not reference columns

--- house-prices-advanced/confirm.py
+++ house-prices-advanced/confirm.py
@@ -1,44 +1,41 @@
 from operator import index
+from pydoc import describe
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
-# index_colは
 def visualizer(train, test, purpose_value):
-#目的変数のヒストグラム
-  purpose_value = sns.histplot(train[purpose_value]) 
-  purpose_value.set_title("histgram of purpose value")
-  tmp = purpose_value.get_figure()
-  tmp.savefig('purpose_calue_histgram.png',  orientation="landscape")
+  new_dir_path = 'data/temp/new-dir'
+  os.mkdir(new_dir_path)
 
+
+#目的変数のヒストグラム
+  purpose_value_hist = sns.histplot(train[purpose_value]) 
+  purpose_value_hist.set_title("histgram of purpose value")
+  tmp = purpose_value_hist.get_figure()
+  tmp.savefig('_purpose_calue_histgram.png',  orientation="landscape")
 
 # 目的変数の概要
-  file = open('purpose_value.txt', 'w')
-  file.write("概要\n")
-  file.write(str(train["SalePrice"].describe()))
+  file = open('_purpose.txt', 'w')
+  file.write("目的変数の概要\n")
+  file.write(str(train[purpose_value].describe()))
   file.write("\n\n尖度と歪度（どれくらい正規分布から離れているか）\n")
-  file.write(f"歪度: {round(train['SalePrice'].skew(),4)}\n" )
-  file.write(f"尖度: {round(train['SalePrice'].kurt(),4)}" )
+  file.write(f"歪度: {round(train[purpose_value].skew(),4)}\n")
+  file.write(f"尖度: {round(train[purpose_value].kurt(),4)}")
   file.close()
 
-# 欠損値の割合を見る
-# print(df_train.isnull().sum())
-  # plt.savefig("histgram")
-  # pd.set_option("display.max_columns", 50)
-  # file = open('frame_train.txt', 'w')
-  # file.write("\n")
-  # file.write(str(df_train.head()))
-  # file.close
+  # 説明変数
+  file = open('_train_explain_value.txt', 'w')
+  N, D = train.shape
+  pd.set_option('display.max_rows', N)
+  pd.set_option('display.max_columns', D)
+  file.write("説明変数の欠損割合\n")
+  file.write(str(train.isnull().sum()))
+  file.close()
 
-
-# file.write("\n")
-# file.write(ans)
-# file.write("\n\n尖度や歪度\n\n")
-# file.write(ans2)
-# file.close()
 if __name__ == '__main__':
   df_train = pd.read_csv("train.csv")
-  # print(df_train['SalePrice'])
   df_test =pd.read_csv("test.csv")
   visualizer(df_train, df_test, "SalePrice")
 
