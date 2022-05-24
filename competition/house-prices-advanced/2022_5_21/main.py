561e7c095e2671fc662563132f583292b6a334fb
nakashima1125 <free.nakashima@gmail.com>
2022-05-24 21:41:10 +0900

    add model

--- competition/house-prices-advanced/2022_5_21/main.py
+++ competition/house-prices-advanced/2022_5_21/main.py
@@ -1,12 +1,66 @@
 from sklearn.linear_model import Lasso 
 import pandas as pd
 from prior_process import prior_process
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import (
+    LinearRegression,
+    Ridge,
+    Lasso
+)
+import matplotlib.pyplot as plt
+
+
+
 def main(train, test):
-  
+  # 学習データ内の分割
+  train_x = p_train.drop('SalePrice',axis=1)
+  train_y = np.log(p_train['SalePrice'])
 
+  # テストデータ内の分割
+  test_id = test['Id']
+  test_data = test.drop('Id',axis=1)
 
+  scaler = StandardScaler()  #スケーリング
+
+  param_grid = [0.001, 0.01, 0.1, 1.0, 10.0,100.0,1000.0] #パラメータグリッド
+  cnt = 0
+
+  for alpha in param_grid:
+
+    ls = Lasso(alpha=alpha) #Lasso回帰モデル
+
+    pipeline = make_pipeline(scaler, ls) #パイプライン生成
+
+    X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.3, random_state=0)
+
+    pipeline.fit(X_train,y_train)
+
+    train_rmse = np.sqrt(mean_squared_error(y_train, pipeline.predict(X_train)))
+
+    test_rmse = np.sqrt(mean_squared_error(y_test, pipeline.predict(X_test)))
+
+    if cnt == 0:
+        best_score = test_rmse
+        best_estimator = pipeline
+        best_param = alpha
+    elif best_score > test_rmse:
+        best_score = test_rmse
+        best_estimator = pipeline
+        best_param = alpha
+    else:
+        pass
+    cnt = cnt + 1
+    
+  print('alpha : ' + str(best_param))
+  print('test score is : ' +str(best_score))
+ 
 if __name__ == '__main__':
   df_train = pd.read_csv("../train.csv")
   df_test =pd.read_csv("../test.csv")
   chg_s_list = ['MSSubClass','YrSold','MoSold']
   p_train, p_test = prior_process(df_train, df_test, chg_s_list)
+  main(p_train, p_test)
--- competition/house-prices-advanced/2022_5_21/prior_process.py
+++ competition/house-prices-advanced/2022_5_21/prior_process.py
@@ -10,28 +10,29 @@ sys.path.append("../../../")
 from library.visualizer.visualizer import visualizer
 
 def prior_process(train, test, chg_s_list):
-  # 目的変数のSales Priceを除いたtrainとtestを行方向に合成（columを統一化）
+  train['WhatIsData'] = 'Train'
+  test['WhatIsData'] = 'Test'
+  test['SalePrice'] = 9999999999
   alldata = pd.concat([train,test],axis=0).reset_index(drop=True)
-                      
-  #  # 意味のない数字を文字列へ変換
-  # for column in chg_s_list:
-  #   train[column] = train[column].astype(str)
 
-  #  カテゴリカル変数の欠損には欠損を示す文字列’NA’を補完
-  #  数値型変数の欠損には0.0を補完
-  nan_col_list = train.isnull().sum()[train.isnull().sum()>0].index.tolist() # 欠損を含むカラムをリスト化
-  nan_float_cols = (train[nan_col_list].dtypes=='float64').index.tolist() #float64
-  nan_obj_cols = (train[nan_col_list].dtypes=='object').index.tolist() #object
+# 訓練データ特徴量をリスト化
+  cat_cols = alldata.dtypes[train.dtypes=='object'].index.tolist()
+  num_cols = alldata.dtypes[train.dtypes!='object'].index.tolist()
 
-  # float64型で欠損している場合は0を代入
-  for nan_float_col in nan_float_cols:
-  #locで欠損部分の位置を特定し、0.0を代入(行名, 列名)
-    train.loc[train[nan_float_col].isnull(), nan_float_col] = 0.0
-  # object型で欠損している場合は'NA'を代入
-  for nan_obj_col in nan_obj_cols:
-    train.loc[train[nan_obj_col].isnull(), nan_obj_col] = 'NA'
+  other_cols = ['Id','WhatIsData']
+# 余計な要素をリストから削除
+  cat_cols.remove('WhatIsData') #学習データ・テストデータ区別フラグ除去
+  num_cols.remove('Id') #Id削除
+
+  cat = pd.get_dummies(alldata[cat_cols])
+
+# データ統合
+  all_data = pd.concat([alldata[other_cols],alldata[num_cols].fillna(0),cat],axis=1)
+
+  p_train = all_data[all_data['WhatIsData']=='Train'].drop(['WhatIsData','Id'], axis=1).reset_index(drop=True)
+  p_test = all_data[all_data['WhatIsData']=='Test'].drop(['WhatIsData','SalePrice'], axis=1).reset_index(drop=True)
     
-  return train, test
+  return p_train, p_test
 
 if __name__ == '__main__':
   df_train = pd.read_csv("../train.csv")
