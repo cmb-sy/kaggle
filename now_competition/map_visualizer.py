e057a9bf094f36415b5dd1ca267d5eb2d01dcb51
nakashima1125 <free.nakashima@gmail.com>
2022-10-30 18:12:10 +0900

    revise text visualizer

---
+++ now_competition/map_visualizer.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def map_visualizer(df_train, df_test):
+
+
+
+if __name__ =='__main__':
+  df_train = pd.read_csv("data/train.csv")
+  df_test =pd.read_csv('data/test.csv')
+  map_visualizer( df_train, df_test)
---
+++ now_competition/text_visualizer.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os, shutil, io
+
+def text_visualizer(directory_path, df_train, df_test):
+  if os.path.exists(directory_path): # ディレクトリが存在するなら、あるものを削除して新規に作成
+    shutil.rmtree(directory_path) # ディレクトリを中身ごと削除
+    os.mkdir(directory_path) 
+  else: #ディレクトリがないなら新規に作成
+    os.mkdir(directory_path) 
+  
+  file = open(directory_path + '/train_describe.txt', 'w')
+  file.write("< 説明変数の概要 >\n")
+  buffer = io.StringIO()
+  df_train.info(buf=buffer)
+  df_info = buffer.getvalue().split("\n")
+  for i in df_info: 
+    file.write(i + "\n")
+  file.write(str(df_train.shape))
+  
+  file.write("\n\n")
+  file.write("< 説明変数の基本統計量 >\n")
+  file.write("シェイプ" + str(df_train.describe()))
+
+  file.write("\n\n")
+  file.write("< 欠損値の確認 >\n")
+  train_num = df_train.isnull().sum()[df_train.isnull().sum()>0]
+  test_num = df_test.isnull().sum()[df_test.isnull().sum()>0]
+  file.write("train" + str(train_num))
+  file.write("test" + str(test_num))
+  file.close()
+
+
+if __name__ =='__main__':
+  df_train = pd.read_csv("data/train.csv")
+  df_test =pd.read_csv('data/test.csv')
+  text_visualizer(directory_file:='visualfile', df_train, df_test)
