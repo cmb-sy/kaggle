4efe7c1212fa4cc697eb13aaa7e95b820a39880c
nakashima1125 <free.nakashima@gmail.com>
2022-11-03 20:07:03 +0900

    add visualize for hist

--- now_competition/Readme.md
+++ now_competition/Readme.md
@@ -4,18 +4,22 @@ https://www.kaggle.com/code/jocker3/feedback-prize-english-language-learning
 8~12年生の英語学習者の言語能力を評価すること。
 
 ### 手法
-目的変数であるfull_textから、下記のカラムの熟練度を予測する。
+full_textから、カラムの熟練度を予測する。
 評価指標としてMCRMSEを用いる。
 
 ### カラム
 数値が高いほど熟練度が高いということ
 full_text : 全文
-cohesion : 凝集
-syntax : 構文
-vocabulary : 単語
-phraseology : 言い回し
-grammar : 文法
-conventions : 慣例
+cohesion : 凝集（目的変数）
+syntax : 構文（目的変数）
+vocabulary : 単語（目的変数）
+phraseology : 言い回し（目的変数）
+grammar : 文法（目的変数）
+conventions : 慣例（目的変数）
+
+### 考え
+目的変数が文字列であり、種類はデータ数N個ある。
+それに対するデータ加工を考える必要がある。
 
 ### その他
 期限 : 11/30
--- now_competition/map_visualizer.py
+++ now_competition/map_visualizer.py
@@ -1,9 +1,15 @@
+# https://www.kaggle.com/code/yuriao/english-language-learning-lgbm-doc2vec
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from matplotlib.gridspec import GridSpec
 
 def map_visualizer(df_train, df_test):
+  purpose_col=list(df_train.columns)
+  purpose_col.remove('text_id')
+  purpose_col.remove('full_text')
+
   fig = plt.figure(figsize=(16, 12))
   gs = GridSpec(nrows=3, ncols=2, height_ratios=[1, 1, 1])
   axes = [fig.add_subplot(gs[0, 0]),
@@ -12,10 +18,11 @@ def map_visualizer(df_train, df_test):
           fig.add_subplot(gs[1, 1]),
           fig.add_subplot(gs[2, 0]),
           fig.add_subplot(gs[2, 1]),]
+
+  for i, col in enumerate(purpose_col):
+    axes[i].hist(df_train[col])
+    axes[i].set_title(col)    
   
-  col_list = ['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']
-  for i, col in enumerate(col_list):
-    sns.distplot(df_train[col], ax = axes[i], )
   plt.subplots_adjust(wspace=0.2, hspace=0.5) 
   plt.show()
     
